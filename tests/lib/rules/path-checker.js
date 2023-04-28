681c3d8faf439bc40a550c63bbc75123265352c4
Mikhail Barkov <barkovm@yahoo.com>
2023-04-29 00:08:12 +0400

    Initial commit

---
+++ tests/lib/rules/path-checker.js
@@ -0,0 +1,48 @@
+/**
+ * @fileoverview feature sliced relative path checker
+ * @author mikhail
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/path-checker"),
+  RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+});
+ruleTester.run("path-checker", rule, {
+  valid: [
+    {
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+      code: "import { getAddCommentFormError, getAddCommentFormText } from '../../model/slices/addCommentFormSelectors'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительными" }],
+    },
+  ],
+
+  invalid: [
+    {
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+      code: "import { getAddCommentFormError, getAddCommentFormText } from '@/entities/Article/model/slices/addCommentFormSelectors'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительными" }],
+      options: [
+        {
+          alias: '@'
+        }
+      ]
+    },
+    {
+      filename: 'C:\\Users\\tim\\Desktop\\javascript\\production_project\\src\\entities\\Article',
+      code: "import { getAddCommentFormError, getAddCommentFormText } from 'entities/Article/model/slices/addCommentFormSelectors'",
+      errors: [{ message: "В рамках одного слайса все пути должны быть относительными" }],
+    },
+  ],
+});
